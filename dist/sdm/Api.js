f6ed7e307fe5b60c11a0302ed903d6357d5c8ad7
Danny Zolp <danny@zolp.dev>
2023-10-14 12:13:12 -0500

    fix: updated pubsub api for new sdk

--- dist/sdm/Api.js
+++ dist/sdm/Api.js
@@ -40,16 +40,17 @@ class SmartDeviceManagement {
         this.oauth2Client.setCredentials({
             refresh_token: config.refreshToken
         });
-        this.smartdevicemanagement = new google.smartdevicemanagement_v1.Smartdevicemanagement({
-            auth: this.oauth2Client
-        });
+        this.smartdevicemanagement =
+            new google.smartdevicemanagement_v1.Smartdevicemanagement({
+                auth: this.oauth2Client
+            });
         try {
             this.pubSubClient = new pubsub.PubSub({
                 //use GCP project ID if it's present
                 projectId: config.gcpProjectId || config.projectId,
                 credentials: {
                     // @ts-ignore
-                    type: 'authorized_user',
+                    type: "authorized_user",
                     // @ts-ignore
                     client_id: config.clientId,
                     // @ts-ignore
@@ -59,29 +60,29 @@ class SmartDeviceManagement {
                 }
             });
             this.subscription = this.pubSubClient.subscription(config.subscriptionId);
-            this.subscription.on('message', message => {
+            this.subscription.on("message", (message) => {
                 message.ack();
                 if (!this.devices)
                     return;
-                this.log.debug('Event received: ' + message.data.toString());
-                const event = JSON.parse(message.data);
+                this.log.debug("Event received: " + message.data.toString());
+                const event = JSON.parse(message.data.toString());
                 // if ((event as Events.ResourceRelationEvent).relationUpdate) {
                 //     const resourceRelationtEvent = event as Events.ResourceRelationEvent;
                 // } else
                 if (event.resourceUpdate.events) {
                     const resourceEventEvent = event;
-                    const device = lodash_1.default.find(this.devices, device => device.getName() === resourceEventEvent.resourceUpdate.name);
+                    const device = lodash_1.default.find(this.devices, (device) => device.getName() === resourceEventEvent.resourceUpdate.name);
                     if (device)
                         device.event(resourceEventEvent);
                 }
                 else if (event.resourceUpdate.traits) {
                     const resourceTraitEvent = event;
-                    const device = lodash_1.default.find(this.devices, device => device.getName() === resourceTraitEvent.resourceUpdate.name);
+                    const device = lodash_1.default.find(this.devices, (device) => device.getName() === resourceTraitEvent.resourceUpdate.name);
                     if (device)
                         device.event(resourceTraitEvent);
                 }
             });
-            this.subscription.on('error', error => {
+            this.subscription.on("error", (error) => {
                 this.log.error("Plugin initialization failed, there was a failure with event subscription. Did you read the readme: https://github.com/potmat/homebridge-google-nest-sdm#where-do-the-config-values-come-from", error);
                 this.subscribed = false;
             });
@@ -95,19 +96,21 @@ class SmartDeviceManagement {
         if (!this.subscribed)
             return this.devices;
         try {
-            const response = await this.smartdevicemanagement.enterprises.devices.list({ parent: `enterprises/${this.projectId}` });
-            this.log.debug('Receieved list of devices: ', response.data.devices);
+            const response = await this.smartdevicemanagement.enterprises.devices.list({
+                parent: `enterprises/${this.projectId}`
+            });
+            this.log.debug("Receieved list of devices: ", response.data.devices);
             this.devices = (0, lodash_1.default)(response.data.devices)
-                .filter(device => device.name !== null)
-                .map(device => {
+                .filter((device) => device.name !== null)
+                .map((device) => {
                 switch (device.type) {
-                    case 'sdm.devices.types.DOORBELL':
+                    case "sdm.devices.types.DOORBELL":
                         return new Doorbell_1.Doorbell(this.smartdevicemanagement, device, this.log);
-                    case 'sdm.devices.types.CAMERA':
+                    case "sdm.devices.types.CAMERA":
                         return new Camera_1.Camera(this.smartdevicemanagement, device, this.log);
-                    case 'sdm.devices.types.DISPLAY':
+                    case "sdm.devices.types.DISPLAY":
                         return new Display_1.Display(this.smartdevicemanagement, device, this.log);
-                    case 'sdm.devices.types.THERMOSTAT':
+                    case "sdm.devices.types.THERMOSTAT":
                         return new Thermostat_1.Thermostat(this.smartdevicemanagement, device, this.log);
                     default:
                         return new UnknownDevice_1.UnknownDevice(this.smartdevicemanagement, device, this.log);
@@ -116,7 +119,7 @@ class SmartDeviceManagement {
                 .value();
         }
         catch (error) {
-            this.log.error('Could not execute device LIST request: ', JSON.stringify(error));
+            this.log.error("Could not execute device LIST request: ", JSON.stringify(error));
         }
         return this.devices;
     }
--- src/sdm/Api.ts
+++ src/sdm/Api.ts
@@ -1,123 +1,147 @@
-import _ from 'lodash';
-import * as google from 'googleapis';
-import * as pubsub from '@google-cloud/pubsub';
-import {Logger} from 'homebridge';
-import {Config} from "../Config";
-import * as Events from './Events';
-import {Device} from "./Device";
-import {Camera} from "./Camera";
-import {Doorbell} from "./Doorbell";
-import {Thermostat} from "./Thermostat";
-import {UnknownDevice} from "./UnknownDevice";
-import {Display} from "./Display";
+import _ from "lodash";
+import * as google from "googleapis";
+import * as pubsub from "@google-cloud/pubsub";
+import { Logger } from "homebridge";
+import { Config } from "../Config";
+import * as Events from "./Events";
+import { Device } from "./Device";
+import { Camera } from "./Camera";
+import { Doorbell } from "./Doorbell";
+import { Thermostat } from "./Thermostat";
+import { UnknownDevice } from "./UnknownDevice";
+import { Display } from "./Display";
 
 export class SmartDeviceManagement {
-    private oauth2Client: google.Auth.OAuth2Client;
-    private smartdevicemanagement: google.smartdevicemanagement_v1.Smartdevicemanagement;
-    private pubSubClient: pubsub.PubSub | undefined;
-    private subscription: pubsub.Subscription | undefined;
-    private projectId: string;
-    private log: Logger;
-    private devices: Device[] | undefined;
-    private subscribed = true;
+  private oauth2Client: google.Auth.OAuth2Client;
+  private smartdevicemanagement: google.smartdevicemanagement_v1.Smartdevicemanagement;
+  private pubSubClient: pubsub.PubSub | undefined;
+  private subscription: pubsub.Subscription | undefined;
+  private projectId: string;
+  private log: Logger;
+  private devices: Device[] | undefined;
+  private subscribed = true;
 
-    constructor(config: Config, log: Logger) {
-        this.log = log;
+  constructor(config: Config, log: Logger) {
+    this.log = log;
 
-        this.oauth2Client = new google.Auth.OAuth2Client(
-            config.clientId,
-            config.clientSecret
+    this.oauth2Client = new google.Auth.OAuth2Client(
+      config.clientId,
+      config.clientSecret
+    );
+    this.projectId = config.projectId;
+    this.oauth2Client.setCredentials({
+      refresh_token: config.refreshToken
+    });
+    this.smartdevicemanagement =
+      new google.smartdevicemanagement_v1.Smartdevicemanagement({
+        auth: this.oauth2Client
+      });
+
+    try {
+      this.pubSubClient = new pubsub.PubSub({
+        //use GCP project ID if it's present
+        projectId: config.gcpProjectId || config.projectId,
+        credentials: {
+          // @ts-ignore
+          type: "authorized_user",
+          // @ts-ignore
+          client_id: config.clientId,
+          // @ts-ignore
+          client_secret: config.clientSecret,
+          // @ts-ignore
+          refresh_token: config.refreshToken
+        }
+      });
+      this.subscription = this.pubSubClient.subscription(config.subscriptionId);
+      this.subscription.on("message", (message) => {
+        message.ack();
+
+        if (!this.devices) return;
+
+        this.log.debug("Event received: " + message.data.toString());
+
+        const event: Events.Event = JSON.parse(message.data.toString());
+
+        // if ((event as Events.ResourceRelationEvent).relationUpdate) {
+        //     const resourceRelationtEvent = event as Events.ResourceRelationEvent;
+        // } else
+        if ((event as Events.ResourceEventEvent).resourceUpdate.events) {
+          const resourceEventEvent = event as Events.ResourceEventEvent;
+          const device = _.find(
+            this.devices,
+            (device) =>
+              device.getName() === resourceEventEvent.resourceUpdate.name
+          );
+          if (device) device.event(resourceEventEvent);
+        } else if ((event as Events.ResourceTraitEvent).resourceUpdate.traits) {
+          const resourceTraitEvent = event as Events.ResourceTraitEvent;
+          const device = _.find(
+            this.devices,
+            (device) =>
+              device.getName() === resourceTraitEvent.resourceUpdate.name
+          );
+          if (device) device.event(resourceTraitEvent);
+        }
+      });
+      this.subscription.on("error", (error) => {
+        this.log.error(
+          "Plugin initialization failed, there was a failure with event subscription. Did you read the readme: https://github.com/potmat/homebridge-google-nest-sdm#where-do-the-config-values-come-from",
+          error
         );
-        this.projectId = config.projectId;
-        this.oauth2Client.setCredentials({
-            refresh_token: config.refreshToken
+        this.subscribed = false;
+      });
+    } catch (error: any) {
+      this.log.error(
+        "Plugin initialization failed, there was a failure with event subscription. Did you read the readme: https://github.com/potmat/homebridge-google-nest-sdm#where-do-the-config-values-come-from",
+        error
+      );
+      this.subscribed = false;
+    }
+  }
+
+  async list_devices(): Promise<Device[] | undefined> {
+    if (!this.subscribed) return this.devices;
+
+    try {
+      const response =
+        await this.smartdevicemanagement.enterprises.devices.list({
+          parent: `enterprises/${this.projectId}`
         });
-        this.smartdevicemanagement = new google.smartdevicemanagement_v1.Smartdevicemanagement({
-            auth: this.oauth2Client
-        });
 
-        try {
-            this.pubSubClient = new pubsub.PubSub({
-                //use GCP project ID if it's present
-                projectId: config.gcpProjectId || config.projectId,
-                credentials: {
-                    // @ts-ignore
-                    type: 'authorized_user',
-                    // @ts-ignore
-                    client_id: config.clientId,
-                    // @ts-ignore
-                    client_secret: config.clientSecret,
-                    // @ts-ignore
-                    refresh_token: config.refreshToken
-                }
-            });
-            this.subscription = this.pubSubClient.subscription(config.subscriptionId);
-            this.subscription.on('message', message => {
-                message.ack();
+      this.log.debug("Receieved list of devices: ", response.data.devices);
 
-                if (!this.devices)
-                    return;
-
-                this.log.debug('Event received: ' + message.data.toString());
-
-                const event: Events.Event = JSON.parse(message.data);
-
-                // if ((event as Events.ResourceRelationEvent).relationUpdate) {
-                //     const resourceRelationtEvent = event as Events.ResourceRelationEvent;
-                // } else
-                if ((event as Events.ResourceEventEvent).resourceUpdate.events) {
-                    const resourceEventEvent = event as Events.ResourceEventEvent;
-                    const device = _.find(this.devices, device => device.getName() === resourceEventEvent.resourceUpdate.name);
-                    if (device)
-                        device.event(resourceEventEvent);
-                } else if ((event as Events.ResourceTraitEvent).resourceUpdate.traits) {
-                    const resourceTraitEvent = event as Events.ResourceTraitEvent;
-                    const device = _.find(this.devices, device => device.getName() === resourceTraitEvent.resourceUpdate.name);
-                    if (device)
-                        device.event(resourceTraitEvent);
-                }
-            });
-            this.subscription.on('error', error => {
-                this.log.error("Plugin initialization failed, there was a failure with event subscription. Did you read the readme: https://github.com/potmat/homebridge-google-nest-sdm#where-do-the-config-values-come-from", error);
-                this.subscribed = false;
-            });
-        } catch (error: any) {
-            this.log.error("Plugin initialization failed, there was a failure with event subscription. Did you read the readme: https://github.com/potmat/homebridge-google-nest-sdm#where-do-the-config-values-come-from", error);
-            this.subscribed = false;
-        }
+      this.devices = _(response.data.devices)
+        .filter((device) => device.name !== null)
+        .map((device) => {
+          switch (device.type) {
+            case "sdm.devices.types.DOORBELL":
+              return new Doorbell(this.smartdevicemanagement, device, this.log);
+            case "sdm.devices.types.CAMERA":
+              return new Camera(this.smartdevicemanagement, device, this.log);
+            case "sdm.devices.types.DISPLAY":
+              return new Display(this.smartdevicemanagement, device, this.log);
+            case "sdm.devices.types.THERMOSTAT":
+              return new Thermostat(
+                this.smartdevicemanagement,
+                device,
+                this.log
+              );
+            default:
+              return new UnknownDevice(
+                this.smartdevicemanagement,
+                device,
+                this.log
+              );
+          }
+        })
+        .value();
+    } catch (error: any) {
+      this.log.error(
+        "Could not execute device LIST request: ",
+        JSON.stringify(error)
+      );
     }
 
-    async list_devices(): Promise<Device[] | undefined> {
-
-        if (!this.subscribed)
-            return this.devices;
-
-        try {
-            const response = await this.smartdevicemanagement.enterprises.devices.list({parent: `enterprises/${this.projectId}`})
-
-            this.log.debug('Receieved list of devices: ', response.data.devices)
-
-            this.devices = _(response.data.devices)
-                .filter(device => device.name !== null)
-                .map(device => {
-                    switch (device.type) {
-                        case 'sdm.devices.types.DOORBELL':
-                            return new Doorbell(this.smartdevicemanagement, device, this.log)
-                        case 'sdm.devices.types.CAMERA':
-                            return new Camera(this.smartdevicemanagement, device, this.log)
-                        case 'sdm.devices.types.DISPLAY':
-                            return new Display(this.smartdevicemanagement, device, this.log)
-                        case 'sdm.devices.types.THERMOSTAT':
-                            return new Thermostat(this.smartdevicemanagement, device, this.log)
-                        default:
-                            return new UnknownDevice(this.smartdevicemanagement, device, this.log);
-                    }
-                })
-                .value();
-        } catch (error: any) {
-            this.log.error('Could not execute device LIST request: ', JSON.stringify(error));
-        }
-
-        return this.devices;
-    }
+    return this.devices;
+  }
 }
